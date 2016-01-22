5ab404d380bc4408757cb310e809fa13065959ee
Sarah Wong <sarahlt.wong@outlook.com>
2016-01-21 18:40:02 -0800

    still doesn't work

--- app.js
+++ app.js
@@ -4,22 +4,25 @@ var maxCustomersPerHour = [88,24,38,48,24];
 var avgCookiesSoldPerHour = [5.2,1.2,1.9,3.3,2.6];
 
 var timeArray = ["10am","11am","12pm","1pm","2pm","3pm","4pm","5pm"];
-var customerCountPerHour = [];
+var customerCountPerHour = [[],[],[],[],[]];
 
 var timeList = document.getElementById("printedTimeList");
 
-function randomCustomerGenerator (min,max) {
-  var generatedCustomerCountPerHour = Math.floor((Math.random() * max) + min);
-  customerCountPerHour.push(generatedCustomerCountPerHour);
-  console.log(customerCountPerHour);
-}
+for (var k = 0; k < locations.length; k++) {
 
-for (var i = 0; i < timeArray.length; i++) {
-  randomCustomerGenerator(minCustomersPerHour[0],maxCustomersPerHour[0]);
-}
+  for (var i = 0; i < timeArray.length; i++) {
+    randomCustomerGenerator(minCustomersPerHour[k],maxCustomersPerHour[k]);
 
-for (i in timeArray) {
-  var liEL = document.createElement("li");
-  liEL.textContent = timeArray[i] + ": " + customerCountPerHour[i];
-  timeList.appendChild(liEL);
+    function randomCustomerGenerator (min,max) {
+      var generatedCustomerCountPerHour = Math.floor((Math.random() * max) + min);
+      customerCountPerHour[k,i].push(generatedCustomerCountPerHour);
+      console.log(customerCountPerHour);
+    }
+  }
+
+  for (j in timeArray) {
+    var liEL = document.createElement("li");
+    liEL.textContent = timeArray[j] + ": " + customerCountPerHour[k,j];
+    timeList.appendChild(liEL);
+  }
 }
